9d77cc16945d4831ff4ca215fd621a0762fd870b
Mathis Bichet <mbichet@z2r6p7.42lyon.fr>
2025-12-09 15:34:55 +0100

    add rrb rrr

---
+++ action/rrb.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rrb.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbichet <mbichet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 15:22:45 by mbichet           #+#    #+#             */
+/*   Updated: 2025/12/09 15:23:32 by mbichet          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	rrb(t_stack *b)
+{
+	int i;
+	int temps;
+	
+	i = 0;
+	temps = b->array[0];
+	while (i < b->index_top - 1)
+	{
+		b->array[i] = b->array[i + 1];
+		i++;
+	}
+	b->array[b->index_top] = temps;
+	return (0);
+}
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mbichet <mbichet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 10:11:29 by mbichet           #+#    #+#             */
-/*   Updated: 2025/12/09 14:31:35 by aluslu           ###   ########lyon.fr   */
+/*   Updated: 2025/12/09 15:34:28 by mbichet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ typedef struct s_stack
 int	pa(t_stack *a, t_stack *b);
 void swap_a(t_stack *a);
 int	rra(t_stack *a);
+int	rrb(t_stack *b);
 int	pb(t_stack *a, t_stack *b);
 int	ra(t_stack a);
 int	rb(t_stack *b);
