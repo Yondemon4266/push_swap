969714b2cb9e4e87c9f15b572c86ce3859b23ac3
Mathis Bichet <mbichet@z2r6p7.42lyon.fr>
2025-12-09 14:28:16 +0100

    change chain list to aray

--- action/pa.c
+++ action/pa.c
@@ -6,7 +6,7 @@
 /*   By: mbichet <mbichet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 12:41:21 by mbichet           #+#    #+#             */
-/*   Updated: 2025/12/09 13:59:10 by mbichet          ###   ########lyon.fr   */
+/*   Updated: 2025/12/09 14:27:17 by mbichet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,5 @@
 
 int	pa(t_stack *a, t_stack *b)
 {
-	if (b->previous == 0 || a == NULL || b == NULL)
-		return (0);
-	b = go_back(b);
-	a = add_back(a,b);
-	delete_element(b);
+	return (1);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mbichet <mbichet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 10:11:29 by mbichet           #+#    #+#             */
-/*   Updated: 2025/12/09 14:03:56 by mbichet          ###   ########lyon.fr   */
+/*   Updated: 2025/12/09 14:27:06 by mbichet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_SIZE_ARAY 100
+
 typedef struct s_stack
 {
-	int element;
-	struct s_stack *next;
-	struct s_stack *previous;
-	
+	int	aray[MAX_SIZE_ARAY]	
 }	t_stack;
-
-t_stack	*go_back(t_stack *stack);
-t_stack *add_back(t_stack *lst,t_stack *element);
-int delete_element(t_stack *lst);
 
 #endif
