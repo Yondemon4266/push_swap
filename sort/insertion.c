12776ed38ca0da195efdd3c2431bafc51f62ba9d
Mathis Bichet <mbichet@z2r6p7.42lyon.fr>
2025-12-09 17:57:04 +0100

    add insertion

--- sort/insertion.c
+++ sort/insertion.c
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ra.c                                               :+:      :+:    :+:   */
+/*   insertion.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbichet <mbichet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/09 14:21:40 by mbichet           #+#    #+#             */
-/*   Updated: 2025/12/09 17:24:59 by mbichet          ###   ########lyon.fr   */
+/*   Created: 2025/12/09 17:26:58 by mbichet           #+#    #+#             */
+/*   Updated: 2025/12/09 17:56:31 by mbichet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../push_swap.h"
+#include "push_swap.h"
 
-int	ra(t_stack *a)
+int	insertion(t_stack *a)
 {
-	int i;
-	int temps;
-	
-	i = a->index_top;
-	temps = a->array[a->index_top];
-	while (i >= 0)
-	{
-		a->array[i] = a->array[i];
-		i--;
-	}
-	a->array[0] = temps;
 	return (0);
 }
-
