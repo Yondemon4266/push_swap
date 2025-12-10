b8483759c91c93717a6143740a0440f621651169
Ali Uslu <aluslu@z2r4p8.42lyon.fr>
2025-12-10 16:44:51 +0100

    splitter

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "push_swap.h"
 
-t_stack create_stack(char **av, int length)
+t_stack fill_stack(char **av, int length)
 {
     t_stack stack;
     int i;
@@ -16,18 +16,21 @@ t_stack create_stack(char **av, int length)
     return (stack);
 }
 
+
 int main(int ac, char **av)
 {
     if (ac >= 2)
     {
+        char **arr;
         int length = ac -1;
         t_stack stack_a;
         t_stack stack_b;
-        stack_a = create_stack(av, length);
-        stack_b.array[0] = 1;
-        stack_b.index_top = 0;
-        buble(&stack_a);
-        // while (stack_a.index_top >= 0)
+        
+        arr = ft_split(av[1], ' ');
+        stack_a = fill_stack(av, length);
+        stack_b.index_top = -1;
+        
+                // while (stack_a.index_top >= 0)
         // {
         //     printf("stack a:%d\n", stack_a.array[stack_a.index_top]);
         //     stack_a.index_top--;
