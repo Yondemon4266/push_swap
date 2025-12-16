1825bb438598955eb6c33d4c0b3c96918ea732a6
Ali Uslu <aluslu@z3r3p4.42lyon.fr>
2025-12-16 10:29:31 +0100

    stable...

--- build/action/ra.d
+++
@@ -1,6 +0,0 @@
-build/action/ra.o: action/ra.c action/../push_swap.h \
-  action/../libft/libft.h
-
-action/../push_swap.h:
-
-action/../libft/libft.h:
--- build/sort/buble.d
+++
@@ -1,6 +0,0 @@
-build/sort/buble.o: sort/buble.c sort/../push_swap.h \
-  sort/../libft/libft.h
-
-sort/../push_swap.h:
-
-sort/../libft/libft.h:
