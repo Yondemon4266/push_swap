fd97323278fd110496a0161302ee9299016e6344
Ali Uslu <aluslu@z2r5p7.42lyon.fr>
2025-12-17 10:44:53 +0100

    stable version with 3 algorithms

--- build/action/rr.d
+++
@@ -1,6 +0,0 @@
-build/action/rr.o: action/rr.c action/../push_swap.h \
-  action/../libft/libft.h
-
-action/../push_swap.h:
-
-action/../libft/libft.h:
--- build/parsing/parsing_utils.d
+++
@@ -1,6 +0,0 @@
-build/parsing/parsing_utils.o: parsing/parsing_utils.c \
-  parsing/../push_swap.h parsing/../libft/libft.h
-
-parsing/../push_swap.h:
-
-parsing/../libft/libft.h:
