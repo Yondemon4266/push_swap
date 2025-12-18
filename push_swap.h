4230b0cb4eb45e6dda95d74341ff3a6c94061e15
Ali Uslu <aluslu@z3r4p4.42lyon.fr>
2025-12-18 10:12:55 +0100

    add .h norm

--- push_swap.h
+++ push_swap.h
@@ -6,103 +6,105 @@
 /*   By: aluslu <aluslu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 10:11:29 by mbichet           #+#    #+#             */
-/*   Updated: 2025/12/18 09:39:29 by aluslu           ###   ########.fr       */
+/*   Updated: 2025/12/18 10:01:35 by aluslu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include "libft/libft.h"
+# include <stdarg.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <stdarg.h>
-# include "libft/libft.h"
 
 typedef struct s_stack
 {
-	int	index_top;
-	int	*array;
-	int *ranks;
-}	t_stack;
+	int				index_top;
+	int				*array;
+	int				*ranks;
+}					t_stack;
 
-enum e_strategy {
+enum				e_strategy
+{
 	SIMPLE,
 	MEDIUM,
 	COMPLEX,
-	ADAPTIVE	
+	ADAPTIVE
 };
 
 typedef struct s_bench_infos
 {
-	float	disorder;
+	float			disorder;
 	enum e_strategy	strategy;
-	int	bench;
-	int	total_ops;
-	int	sa_count;
-	int	sb_count;
-	int	ss_count;
-	int	pa_count;
-	int	pb_count;
-	int	ra_count;
-	int	rb_count;
-	int	rr_count;
-	int	rra_count;
-	int	rrb_count;
-	int	rrr_count;
-}	t_bench_infos;
+	int				bench;
+	int				total_ops;
+	int				sa_count;
+	int				sb_count;
+	int				ss_count;
+	int				pa_count;
+	int				pb_count;
+	int				ra_count;
+	int				rb_count;
+	int				rr_count;
+	int				rra_count;
+	int				rrb_count;
+	int				rrr_count;
+}					t_bench_infos;
 
 /* FONCTIONS D'INSTRUCTIONS */
 
-int		pa(t_stack *a, t_stack *b);
-int		pb(t_stack *a, t_stack *b);
-int		ra(t_stack *a);
-int		rb(t_stack *b);
-int		rr(t_stack *a, t_stack *b);
-int		rra(t_stack *a);
-int		rrb(t_stack *b);
-int		rrr(t_stack *a, t_stack *b);
-int		sa(t_stack *a);
-int		sb(t_stack *b);
-int		ss(t_stack *a, t_stack *b);
+int					pa(t_stack *a, t_stack *b);
+int					pb(t_stack *a, t_stack *b);
+int					ra(t_stack *a);
+int					rb(t_stack *b);
+int					rr(t_stack *a, t_stack *b);
+int					rra(t_stack *a);
+int					rrb(t_stack *b);
+int					rrr(t_stack *a, t_stack *b);
+int					sa(t_stack *a);
+int					sb(t_stack *b);
+int					ss(t_stack *a, t_stack *b);
 
 /* FONCTIONS D'ALGORITHME */
 
-void	simple(t_stack *a, t_bench_infos *infos);
-void	medium(t_stack *a, t_stack *b, t_bench_infos *infos);
-void	complex(t_stack *a, t_stack *b, t_bench_infos *infos);
+void				simple(t_stack *a, t_bench_infos *infos);
+void				medium(t_stack *a, t_stack *b, t_bench_infos *infos);
+void				complex(t_stack *a, t_stack *b, t_bench_infos *infos);
 
 /* PARSING && PARSING UTILS */
 
-t_stack	manage_input(char **av, int ac);
-char	*join_strings(char **av, int ac);
-int	count_size_str(char **av, int ac);
-int	get_top_index(char **tab);
-void	fill_string(char *joined_str, char **av, int ac);
-void	str_arrayto_int(char **str, t_stack *stack_a);
-int ft_plus_or_minus(char c);
-int	ft_isspace(char c);
-void    free_str_array(char **str);
-void	swap(int *a, int *b);
-char    *ft_strcpy(char *dest, const char *src);
+t_stack				manage_input(char **av, int ac);
+char				*join_strings(char **av, int ac);
+int					count_size_str(char **av, int ac);
+int					get_top_index(char **tab);
+void				fill_string(char *joined_str, char **av, int ac);
+void				str_arrayto_int(char **str, t_stack *stack_a);
+int					ft_plus_or_minus(char c);
+int					ft_isspace(char c);
+void				free_str_array(char **str);
+void				swap(int *a, int *b);
+char				*ft_strcpy(char *dest, const char *src);
 
 /* FT_PRINTF */
 
-int	ft_printf(const char *str, ...);
-int	ft_putcharl(int c);
-int	ft_putstrl(char *s);
-int	ft_putnbrl(int nb);
-int	ft_unsigned_putnbrl(unsigned int nb);
-int	ft_putnbrl_hexa(uintptr_t nb, char c);
-int	ft_print_pointer(void *ptr);
-char	*ft_strcpy(char *dest, const char *src);
+int					ft_printf(const char *str, ...);
+int					ft_putcharl(int c);
+int					ft_putstrl(char *s);
+int					ft_putnbrl(int nb);
+int					ft_unsigned_putnbrl(unsigned int nb);
+int					ft_putnbrl_hexa(uintptr_t nb, char c);
+int					ft_print_pointer(void *ptr);
+char				*ft_strcpy(char *dest, const char *src);
 
 /* MANAGING FLAGS FUNCTIONS */
 
-int	is_valid_flag(char *flag);
-void	manage_flag(char *flag, enum e_strategy *strategy, int *bench);
+int					is_valid_flag(char *flag);
+void				manage_flag(char *flag, enum e_strategy *strategy,
+						int *bench);
 
 /* DISPLAY INFOS */
 
-void    display_bench(t_bench_infos *infos);
+void				display_bench(t_bench_infos *infos);
 
 #endif
