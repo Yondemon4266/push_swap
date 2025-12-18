fde14c6ac3f00b70079cddeae07c0c5462b563ea
Mathis Bichet <mbichet@z3r4p2.42lyon.fr>
2025-12-18 12:43:10 +0100

    fix benchmarck

--- build/action/sa.d
+++
@@ -1,6 +0,0 @@
-build/action/sa.o: action/sa.c action/../push_swap.h \
-  action/../libft/libft.h
-
-action/../push_swap.h:
-
-action/../libft/libft.h:
--- build/parsing/parsing_utils_two.d
+++
@@ -1,6 +0,0 @@
-build/parsing/parsing_utils_two.o: parsing/parsing_utils_two.c \
-  parsing/../push_swap.h parsing/../libft/libft.h
-
-parsing/../push_swap.h:
-
-parsing/../libft/libft.h:
